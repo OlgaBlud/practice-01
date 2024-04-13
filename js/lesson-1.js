1af5ccf66fa4a99ba59b73897df2e3592c732a8a
victor <157064298+sundnepr@users.noreply.github.com>
2024-04-13 11:07:42 +0300

    Update lesson-1.js

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -13,3 +13,34 @@ function checkHourQuarter(number) {
   } else return "Incorrect number";
 }
 console.log(checkHourQuarter(min));
+
+// 2
+
+//~ Змінна num може набувати 4 значення: 1, 2, 3 або 4. Якщо вона має
+//~ значення '1', то у змінну result запишемо 'зима', якщо має значення
+//~ '2' - 'весна' і так далі. Розв'яжіть завдання через switch-case.
+const s = 5;
+function chekSizon(s) {
+  let sezon = "";
+  switch (s) {
+    case 1:
+      sezon = "зима";
+      break;
+    case 2:
+      sezon = "весна";
+      break;
+    case 3:
+      sezon = "лето";
+      break;
+    case 4:
+      sezon = "осень";
+      break;
+
+    default:
+      sezon = " Enter 1-4";
+      break; 
+  }
+  return sezon;
+}
+
+console.log(chekSizon(s));
